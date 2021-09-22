31c5cadb0cf399f3cab6caa91ddf856cdd47f563
Nicolás Tolaba <nicolasenriquetolaba@gmail.com>
2021-09-22 20:32:47 -0300

    Corregi funciones index

--- main.js
+++ main.js
@@ -1,25 +1,17 @@
-
-function ingresoCantidad(pregunta){
-    var cantidad = prompt(pregunta);
-    var cantidadInt = parseInt(cantidad);
-    return cantidad;
+function preguntaPrecio (){
+    let precio = parseInt(prompt("Ingresa el precio"));
+    return precio;
 }
 
-function preguntaPrecio (preguntaCosto){
-    var entrada = prompt(preguntaCosto);
-    var entradaInt = parseInt(entrada);
-    while(entrada != "no"){
-        entrada = prompt("Ingresar siguiente precio")
-        entradaInt = parseInt(entrada)
+function ingresoCantidad(){
+    let precioTotal = 0;
+    let cantidad = parseInt(prompt("Ingresa la cantidad de hamburguesas"));
+    for(let u = 1; i <= cantidad; i++){
+        precioTotal += preguntaPrecio;
     }
+    return cantidad + " hamburguesas cuestan $" + precioTotal;
 }
 
-function suma (){
-    var total = numeroCan + preguntaPrecio2
-    return total
-}
-
-var numeroCan = ingresoCantidad("ingresa cantidad de hamburguesas")
-var preguntaPrecio2 = preguntaPrecio("Ingrese el precio de las hamburguesas")
-console.log(suma())
+let ticket = ingresoCantidad();
+console.log(ticket);
 
