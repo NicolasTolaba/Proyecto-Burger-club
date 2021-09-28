21ae547c5a5810866710167b9ddf538138814317
Nicolás Tolaba <nicolasenriquetolaba@gmail.com>
2021-09-27 22:16:29 -0300

    añadi arrays

--- main.js
+++ main.js
@@ -1,17 +1,66 @@
-function preguntaPrecio (){
-    let precio = parseInt(prompt("Ingresa el precio"));
-    return precio;
-}
+// function preguntaPrecio (){
+//     let precio = parseInt(prompt("Ingresa el precio"));
+//     return precio;
+// }
+
+// function ingresoCantidad(){
+//     let precioTotal = 0;
+//     let cantidad = parseInt(prompt("Ingresa la cantidad de hamburguesas"));
+//     for(let i = 1; i <= cantidad; i++){
+//         precioTotal += preguntaPrecio();
+//     }
+//     return cantidad + " hamburguesas cuestan $" + precioTotal;
+// }
+
+// let ticket = ingresoCantidad();
+// console.log(ticket);
+
+
+const productos = [
+    {
+        nombre: "Classic burger",
+        precio: 400,
+    },
+    {
+        nombre: "Chicken burger",
+        precio: 400,
+    },
+    {
+        nombre: "Double burger",
+        precio: 500,
+    },
+    {
+        nombre: "Extra chesse burger",
+        precio: 420,
+    },
+    {
+        nombre: "Guacamole burger",
+        precio: 415,
+    },
+    {
+        nombre: "Not burger",
+        precio: 400,
+    },
+]
 
 function ingresoCantidad(){
-    let precioTotal = 0;
     let cantidad = parseInt(prompt("Ingresa la cantidad de hamburguesas"));
-    for(let u = 1; i <= cantidad; i++){
-        precioTotal += preguntaPrecio;
+    for(let i = 1; i <= cantidad; i++){
+        let entrada = prompt("Ingresar nombre de la hamburguesa");
+        if (entrada == productos[i].nombre){
+            let precioTotal = productos[i].precio
+            console.log(precioTotal)
+        }
     }
-    return cantidad + " hamburguesas cuestan $" + precioTotal;
 }
 
-let ticket = ingresoCantidad();
-console.log(ticket);
+ingresoCantidad()
+
+
+
+
+
+
+
+
 
