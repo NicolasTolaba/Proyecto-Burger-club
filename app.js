fc5aa00111176c5a37f09757bb8cfa726a7dd7bb
Nicolás Tolaba <nicolasenriquetolaba@gmail.com>
2021-10-05 13:34:43 -0300

    modifico el DOM a traves de js

---
+++ app.js
@@ -0,0 +1,52 @@
+const burgers = [
+    {
+        img: "img/imagenes/bgclasic-min.jpg",
+        nombre: "Classic burger",
+        precio: 400,
+    },
+    {
+        img: "img/imagenes/bgchicken-min.jpg",
+        nombre: "Chicken burger",
+        precio: 400,
+    },
+    {
+        img: "img/imagenes/bgdoble-min.jpg",
+        nombre: "Double burger",
+        precio: 500,
+    },
+    {
+        img:"img/imagenes/bgquesera-min.jpg",
+        nombre: "Extra chesse burger",
+        precio: 420,
+    },
+    {
+        img: "img/imagenes/bgguacamole-min.jpg",
+        nombre: "Guacamole burger",
+        precio: 415,
+    },
+    {
+        img: "img/imagenes/bgnot-min.jpg",
+        nombre: "Not burger",
+        precio: 400,
+    },
+]
+
+const contenedorProductos = document.getElementById(burgerItem)
+
+burgers.forEach((burgers) => {
+    const img = document.createElement("img")
+    img.className = "img"
+
+    img.innerHTML = `
+            <img src=${burgers[0].img} alt="Hamburguesa clásica">
+                                <div class="contenidoCarta">
+                                    <h2>${burgers[0].nombre}</h2>
+                                    <h4>Hamburguesa con un medallon de carne (100gr), feta de queso cheddar, lechuga, tomate, pepinos y salsa burgerclub.</h4>
+                                </div>
+                                <div id="precio">
+                                    <p>$${burgers[0].precio}</p>
+                                </div>
+                                <button class="compra">AGREGAR AL CARRITO</button>`
+
+})
+
